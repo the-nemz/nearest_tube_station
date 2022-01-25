8898fa693383c0c331bacdaa8b78b644a6891e82
Isaac Nemzer <ijnemzer@outlook.com>
2022-01-25 00:03:33 +0000

    make initial request to TfL API and display station names in ListView

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,134 @@
+import 'dart:async';
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late Future<NearestStationResponse> futureNearestStationResponse;
+
+  @override
+  void initState() {
+    // TODO: query TfL
+    super.initState();
+    futureNearestStationResponse = fetchNearestStations();
+  }
+
+  Future<NearestStationResponse> fetchNearestStations() async {
+    final response = await http.get(Uri.parse(
+        'https://api.tfl.gov.uk/StopPoint/?lat=51.463&lon=-0.114&stopTypes=NaptanMetroStation&radius=2000'));
+
+    if (response.statusCode == 200) {
+      // If the server did return a 200 OK response,
+      // then parse the JSON.
+      return NearestStationResponse.fromJson(jsonDecode(response.body));
+    } else {
+      // If the server did not return a 200 OK response,
+      // then throw an exception.
+      throw Exception('Failed to load album');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Nearest Tube Station'),
+          backgroundColor: Colors.blueGrey,
+        ),
+        body: Center(
+          child: FutureBuilder<NearestStationResponse>(
+            future: futureNearestStationResponse,
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                // return Text('${snapshot.data!.stations[0].name}');
+                return ListView.builder(
+                  itemCount: snapshot.data!.stations.length,
+                  itemBuilder: (_, index) {
+                  return Text( snapshot.data!.stations[index].name);
+                });
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
+              }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+              // By default, show a loading spinner.
+              return const CircularProgressIndicator();
+            },
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.train_outlined),
+              label: 'Rail',
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            BottomNavigationBarItem(
+              icon: Icon(Icons.directions_bus_filled_outlined),
+              label: 'Bus',
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class NearestStationResponse {
+  final int pageSize;
+  final int total;
+  final int page;
+  final List<StationResponse> stations;
+
+  const NearestStationResponse({
+    required this.pageSize,
+    required this.total,
+    required this.page,
+    required this.stations,
+  });
+
+  factory NearestStationResponse.fromJson(Map<String, dynamic> json) {
+    List<StationResponse> stations = [];
+    for (final s in json['stopPoints']) {
+      stations.add(StationResponse(
+        id: s['id'],
+        name: s['commonName'],
+        distance: s['distance'],
+        latitude: s['lat'],
+        longitude: s['lon'],
+      ));
+    }
+
+    return NearestStationResponse(
+      pageSize: json['pageSize'],
+      total: json['total'],
+      page: json['page'],
+      stations: stations,
+    );
+  }
+}
+
+class StationResponse {
+  final String id;
+  final String name;
+  final double distance;
+  final double latitude;
+  final double longitude;
+
+  const StationResponse({
+    required this.id,
+    required this.name,
+    required this.distance,
+    required this.latitude,
+    required this.longitude,
+  });
 }
