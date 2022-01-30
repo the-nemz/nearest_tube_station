71eca3c4b857395d554a82490624a21e78ad39bc
Isaac Nemzer <ijnemzer@outlook.com>
2022-01-30 01:06:11 +0000

    switch from location package to geolocator package

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,9 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'package:location/location.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:http/http.dart' as http;
 
 import 'api/nearest.dart';
 
@@ -22,7 +22,7 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   // late Future<NearestStationResponse> futureNearestStationResponse;
-  LocationData? currentLocation;
+  Position? currentLocation;
   String stopTypes = railStoptypes;
   String? currentLeadStationId;
   int tabIndex = 0;
@@ -30,10 +30,15 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    // TODO: query TfL
     super.initState();
     // futureNearestStationResponse = fetchNearestStations();
-    getLocationData();
+    determinePosition();
+  }
+
+  @override
+  void reassemble() {
+    super.reassemble();
+    determinePosition();
   }
 
   Future<NearestStationResponse?> fetchNearestStations() async {
@@ -69,39 +74,59 @@ class _MyAppState extends State<MyApp> {
     }
   }
 
-  void getLocationData() async {
-    Location location = Location();
-    bool _serviceEnabled;
-    PermissionStatus _permissionGranted;
-    LocationData _locationData;
+  /// Determine the current position of the device.
+  ///
+  /// When the location services are not enabled or permissions
+  /// are denied the `Future` will return an error.
+  void determinePosition() async {
+    bool serviceEnabled;
+    LocationPermission permission;
 
-    _serviceEnabled = await location.serviceEnabled();
-    if (!_serviceEnabled) {
-      _serviceEnabled = await location.requestService();
-      if (!_serviceEnabled) {
-        return;
+    // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
       }
     }
 
-    _permissionGranted = await location.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await location.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        return;
-      }
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
     }
 
-    _locationData = await location.getLocation();
-    print(_locationData);
-    setState(() {
-      currentLocation = _locationData;
-    });
+    // When we reach here, permissions are granted and we can
+    // continue accessing the position of the device.
+    // return await Geolocator.getCurrentPosition();
 
-    location.onLocationChanged.listen((LocationData curr) {
-      print(curr);
+    Position pos = await Geolocator.getCurrentPosition();
+    if (pos != null) {
       setState(() {
-        currentLocation = curr;
+        currentLocation = pos;
       });
+    }
+
+    Geolocator.getPositionStream().listen((Position? position) {
+      if (position != null) {
+        setState(() {
+          currentLocation = position;
+        });
+      }
     });
   }
 
@@ -213,8 +238,8 @@ class _MyAppState extends State<MyApp> {
         floatingActionButton: FloatingActionButton(
             child: const Icon(Icons.refresh),
             backgroundColor: Colors.deepOrange,
-            onPressed: () {
-              getLocationData();
+            onPressed: () async {
+              determinePosition();
             }),
         bottomNavigationBar: BottomNavigationBar(
           items: const [
